06f5875e2777fff2f65d8e308dba950581cc21df
Mario <mario.gonzalez@live.u-tad.com>
2025-02-03 13:42:27 +0100

    styles

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,36 +1,68 @@
 <template>
     <h1>{{ titulo }}</h1>
 
-    <div class="products-container" v-for="producto in productos">
-        <p>{{ producto.id }} - {{ producto.title }}</p>
-    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Ver</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="producto in productos" :key="producto.id">
+                <td>{{ producto.id }}</td>
+                <td>{{ producto.title }}</td>
+                <td>${{ producto.price }}</td>
+                <td>
+                    <button class="ver" @click="verProducto(producto)">
+                        Ver
+                    </button>
+                </td>
+                <td>
+                    <button class="editar" @click="editarProducto(producto)">
+                        Editar
+                    </button>
+                </td>
+                <td>
+                    <button
+                        class="eliminar"
+                        @click="eliminarProducto(producto.id)"
+                    >
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from "axios";
 
-    export default{
-        data(){
-            return{
-                titulo: "Productos",
-                productos: []
-            };
-        },
-        mounted(){
-            this.getProductos();
-        },
+export default {
+    data() {
+        return {
+            titulo: "Productos",
+            productos: [],
+        };
+    },
+    mounted() {
+        this.getProductos();
+    },
 
-        methods:{
-            async getProductos(){
-                try{
-                
-                const response = await axios.get('api/products');
+    methods: {
+        async getProductos() {
+            try {
+                const response = await axios.get("api/products");
                 this.productos = response.data;
-
-            }catch(error){
-                console.log('Error al hacer la petición', error);	
-            }
+            } catch (error) {
+                console.log("Error al hacer la petición", error);
             }
         },
-    }
+    },
+};
 </script>
